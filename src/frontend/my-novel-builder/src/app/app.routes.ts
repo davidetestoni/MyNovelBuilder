e619fccee6212cf1dfd3084120af4346ae7d6d3c
Davide Testoni <davide@testoni.net>
2026-02-21 19:38:46 +0100

    Lazily load route components to reduce initial bundle size

--- src/frontend/my-novel-builder/src/app/app.routes.ts
+++ src/frontend/my-novel-builder/src/app/app.routes.ts
@@ -1,13 +1,5 @@
 import { Routes } from '@angular/router';
-import { NovelsComponent } from './pages/novels/novels.component';
 import { LayoutType } from './types/enums/layout-type';
-import { CompendiaComponent } from './pages/compendia/compendia.component';
-import { NovelEditorComponent } from './pages/novel-editor/novel-editor.component';
-import { CompendiumComponent } from './pages/compendium/compendium.component';
-import { PromptsComponent } from './pages/prompts/prompts.component';
-import { NovelSettingsComponent } from './pages/novel-settings/novel-settings.component';
-import { IntegrationsComponent } from './pages/integrations/integrations.component';
-import { ChatsComponent } from './pages/chats/chats.component';
 import { AppRouteData } from './types/router/app-route-data';
 
 const mainLayoutData = {
@@ -23,52 +15,74 @@ export const routes: Routes = [
   },
   {
     path: 'novels',
-    component: NovelsComponent,
+    loadComponent: () =>
+      import('./pages/novels/novels.component').then((m) => m.NovelsComponent),
     data: mainLayoutData,
   },
   {
     path: 'chat',
-    component: ChatsComponent,
+    loadComponent: () =>
+      import('./pages/chats/chats.component').then((m) => m.ChatsComponent),
     data: mainLayoutData,
   },
   {
     path: 'chat/:id',
-    component: ChatsComponent,
+    loadComponent: () =>
+      import('./pages/chats/chats.component').then((m) => m.ChatsComponent),
     data: mainLayoutData,
   },
   {
     path: 'compendia',
-    component: CompendiaComponent,
+    loadComponent: () =>
+      import('./pages/compendia/compendia.component').then(
+        (m) => m.CompendiaComponent,
+      ),
     data: mainLayoutData,
   },
   {
     path: 'novel/:id',
-    component: NovelEditorComponent,
+    loadComponent: () =>
+      import('./pages/novel-editor/novel-editor.component').then(
+        (m) => m.NovelEditorComponent,
+      ),
     data: mainLayoutData, // TODO: Add a layout type for the editor
   },
   {
     path: 'novel/:id/settings',
-    component: NovelSettingsComponent,
+    loadComponent: () =>
+      import('./pages/novel-settings/novel-settings.component').then(
+        (m) => m.NovelSettingsComponent,
+      ),
     data: mainLayoutData,
   },
   {
     path: 'compendium/:id',
-    component: CompendiumComponent,
+    loadComponent: () =>
+      import('./pages/compendium/compendium.component').then(
+        (m) => m.CompendiumComponent,
+      ),
     data: mainLayoutData,
   },
   {
     path: 'compendium/:id/record/:recordId',
-    component: CompendiumComponent,
+    loadComponent: () =>
+      import('./pages/compendium/compendium.component').then(
+        (m) => m.CompendiumComponent,
+      ),
     data: mainLayoutData,
   },
   {
     path: 'prompts',
-    component: PromptsComponent,
+    loadComponent: () =>
+      import('./pages/prompts/prompts.component').then((m) => m.PromptsComponent),
     data: mainLayoutData,
   },
   {
     path: 'integrations',
-    component: IntegrationsComponent,
+    loadComponent: () =>
+      import('./pages/integrations/integrations.component').then(
+        (m) => m.IntegrationsComponent,
+      ),
     data: mainLayoutData,
   },
 ];
