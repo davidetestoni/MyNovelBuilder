f5a96fe4bb18aa2c62b1f9c3a3afa51cf7093e35
Davide Testoni <davide@testoni.net>
2026-09-03 19:37:00 +0200

    Fix test application data isolation

--- src/backend/MyNovelBuilder/MyNovelBuilder.WebApi.Tests/Factories/TestWebApplicationFactory.cs
+++ src/backend/MyNovelBuilder/MyNovelBuilder.WebApi.Tests/Factories/TestWebApplicationFactory.cs
@@ -1,22 +1,19 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
 
 namespace MyNovelBuilder.WebApi.Tests.Factories;
 
 public class TestWebApplicationFactory<TProgram>
     : WebApplicationFactory<TProgram> where TProgram : class
 {
+    private readonly string _dataFolder = Path.Combine(
+        Path.GetTempPath(),
+        Path.GetRandomFileName());
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("test");
-        builder.ConfigureAppConfiguration((_, config) =>
-        {
-            config.AddInMemoryCollection(new Dictionary<string, string>
-            {
-                ["DataFolder"] = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName())
-            }!);
-        });
+        builder.UseSetting("DataFolder", _dataFolder);
         builder.ConfigureServices(services =>
         {
             // TODO: Override services as needed for testing
